bc520bc378f6f2180ea4eb52c1ec2a1a41602512
Shaun Tan <shauntanzongzhi@gmail.com>
2022-03-12 00:14:51 +0800

    Create 'run_all_tests.sh'
    Script to compile everything, run all Tests & checkstyle

---
+++ run_all_tests.sh
@@ -0,0 +1,8 @@
+javac -Xlint:rawtypes,unchecked ./cs2030s/fp/*.java
+javac -Xlint:rawtypes,unchecked *.java
+java Test1 | grep failed;
+java Test2 | grep failed;
+java Test3 | grep failed;
+java Test4 | grep failed;
+java -jar ~cs2030s/bin/checkstyle.jar -c ~cs2030s/bin/cs2030_checks.xml cs2030s/fp/*.java;
+java -jar ~cs2030s/bin/checkstyle.jar -c ~cs2030s/bin/cs2030_checks.xml *.java;
