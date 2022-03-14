55312e91648334e436cd549e591441256fc99a0c
Shaun Tan <shauntanzongzhi@gmail.com>
2022-03-14 22:04:20 +0800

    Add 'Lab5' to 'run_all_tests.sh'

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -4,5 +4,7 @@ java Test1 | grep failed;
 java Test2 | grep failed;
 java Test3 | grep failed;
 java Test4 | grep failed;
+java Lab5
+printf "A\nA-\nA+\nA\nC\nNo such entry\nNo such entry\nNo such entry\n"
 java -jar ~cs2030s/bin/checkstyle.jar -c ~cs2030s/bin/cs2030_checks.xml cs2030s/fp/*.java;
 java -jar ~cs2030s/bin/checkstyle.jar -c ~cs2030s/bin/cs2030_checks.xml *.java;
